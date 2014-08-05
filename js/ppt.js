ba69e5fa748b1bc152e083d91d20d27bd650a483
Jesús Martínez Mendoza <hongo-loco@hotmail.com>
2014-08-05 14:50:21 -0500

    CSS y JS Optimizado
    CSS Usar transform: scale en lugar de width y active
    
    JS Código repetido eliminado, uso de funciones para procesar el numero
    aleatorio y la elección del usuario., hace más ligero.
    Si, usé Switch adentro de switch! en este caso si es necesario.
    Tambien se agregó jQuery.

--- js/ppt.js
+++ js/ppt.js
@@ -1,206 +1,110 @@
+$(document).ready(function(){
+
 var vpiedra
 var vpapel 
 var vtijera
 var vlagarto 
 var vspock 
 
- function aleatorio(minimo, maximo)
+//EVENTO: En cada click ejecuta jugar(), le pasa el boton seleccionado como argumento y crea un numero con aleatorio()
+$('#imgopciones').on('click', 'img', function(){
+  var usuario = this,
+      maquina = aleatorio(0, 4);
+  //console.log(this);
+  //console.log(maquina);
+
+  jugar(usuario, maquina);
+});
+
+function aleatorio(minimo, maximo)
   {
     var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );  
     return numero;
   }
+
+//Obtiene como argumentos la opción del usuario y un valor aleatorio.
+function jugar(usuario, maquina) {
+  var opcionUsuario = usuario.className;
+
+  switch(opcionUsuario)
+  {
+    case 'piedra': document.getElementById("usuario").innerHTML = "<img src='img/piedra.png'/>"; procesar(maquina, 0); break;
+    case 'papel': document.getElementById("usuario").innerHTML = "<img src='img/papel.png'/>"; procesar(maquina, 1); break;
+    case 'tijera': document.getElementById("usuario").innerHTML = "<img src='img/tijera.png'/>"; procesar(maquina, 2); break;
+    case 'lagarto': document.getElementById("usuario").innerHTML = "<img src='img/lagarto.png'/>"; procesar(maquina, 3); break;
+    case 'spock': document.getElementById("usuario").innerHTML = "<img src='img/spock.png'/>"; procesar(maquina, 4); break;
+  }
+}
+
+/* Piedra = 0, Papel = 1, Tijeras = 2, Lagarto = 3, Spock = 4
+  Recibe la opciones de usuario y maquina, y las compara para declarar un ganador */
+function procesar(opcionMaquina, opcionUsuario) {
+  console.log(opcionUsuario+' vs '+opcionMaquina);
+  document.getElementById("vs").innerHTML = "<img src='img/vs.png'/>";
   
-var opcionMaquina = aleatorio(0,4);
-
-  function PiedraFunction() {
-       document.getElementById("usuario").innerHTML = "<img src='img/piedra.png'/>";
-       document.getElementById("vs").innerHTML = "<img src='img/vs.png'/>";
-       function aleatorio(minimo, maximo){
-            var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );  
-            return numero;
-       }
-      var opcionMaquina = aleatorio(0,4);
-       if (opcionMaquina == 0) {
-        document.getElementById("maquina").innerHTML = "<img  src='img/piedra.png'/>";
-        var vpiedra=0;
-       }else if (opcionMaquina == 1) {
-        document.getElementById("maquina").innerHTML = "<img  src='img/papel.png'/>";
-        var vpapel=1;
-       }else if (opcionMaquina == 2) {
-        document.getElementById("maquina").innerHTML = "<img  src='img/tijera.png'/>";
-        var vtijera=2;
-       }else if (opcionMaquina == 3) {
-        document.getElementById("maquina").innerHTML = "<img src='img/lagarto.png'/>";
-        var vlagarto = 3;
-       }else if (opcionMaquina == 4) {
-        document.getElementById("maquina").innerHTML = "<img src='img/spock.png'/>";
-        var vspock = 4;
+  switch(opcionMaquina)
+  {
+    case 0: document.getElementById("maquina").innerHTML = "<img  src='img/piedra.png'/>"; break;
+    case 1: document.getElementById("maquina").innerHTML = "<img  src='img/papel.png'/>"; break;
+    case 2: document.getElementById("maquina").innerHTML = "<img  src='img/tijera.png'/>"; break;
+    case 3: document.getElementById("maquina").innerHTML = "<img  src='img/lagarto.png'/>"; break;
+    case 4: document.getElementById("maquina").innerHTML = "<img  src='img/spock.png'/>"; break; 
+  }
+  //Si hay empate ya no procesa.
+  if(opcionMaquina == opcionUsuario)
+  {
+    document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/empate.png'/>"; 
+    console.log('Es un empate');
+    return true;
+  } else {
+    switch(opcionUsuario)
+    {
+      case 0: 
+        switch(opcionMaquina)
+        {
+          case 1: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
+          case 2: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+          case 3: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+          case 4: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
         }
-
-        if (vpiedra == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/empate.png'/>";
-        } else if (vpapel == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>";
-        } else if (vtijera == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>";
-        } else if (vlagarto == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>";
-        } else if (vspock == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; 
-        }     
+        break;
+      case 1:
+        switch(opcionMaquina)
+        {
+          case 0: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+          case 2: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
+          case 3: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
+          case 4: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+        }
+        break;
+      case 2:
+        switch(opcionMaquina)
+        {
+          case 0: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
+          case 1: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+          case 3: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+          case 4: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
+        }
+        break;
+      case 3:
+        switch(opcionMaquina)
+        {
+          case 0: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
+          case 1: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+          case 2: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
+          case 4: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+        }
+        break;
+      case 4:      
+        switch(opcionMaquina)
+        {
+          case 0: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+          case 1: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
+          case 2: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; break;
+          case 3: document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; break;
+        }
     }
-
-  function PapelFunction() {
-       document.getElementById("usuario").innerHTML = "<img src='img/papel.png'/>";
-       document.getElementById("vs").innerHTML = "<img src='img/vs.png'/>";
-       function aleatorio(minimo, maximo){
-            var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );  
-            return numero;
-       }
-       var opcionMaquina = aleatorio(0,4);
-       if (opcionMaquina == 0) {
-        document.getElementById("maquina").innerHTML = "<img src='img/piedra.png'/>";
-        var vpiedra=0;
-       }else if (opcionMaquina == 1) {
-        document.getElementById("maquina").innerHTML = "<img src='img/papel.png'/>";
-        var vpapel=1;
-       }else if (opcionMaquina == 2) {
-        document.getElementById("maquina").innerHTML = "<img src='img/tijera.png'/>";
-        var vtijera=2;
-       }else if (opcionMaquina == 3) {
-        document.getElementById("maquina").innerHTML = "<img src='img/lagarto.png'/>";
-        var vlagarto = 3;
-       }else if (opcionMaquina == 4) {
-        document.getElementById("maquina").innerHTML = "<img src='img/spock.png'/>";
-        var vspock = 4;
-        }
-
-        if (vpiedra == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>";
-        } else if (vpapel == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/empate.png'/>";
-        } else if (vtijera == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>";
-        } else if (vlagarto == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>";
-        } else if (vspock == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>";
-        }     
-       
   }
-    
-  function TijeraFunction() {
-       document.getElementById("usuario").innerHTML = "<img src='img/tijera.png'/>";
-       document.getElementById("vs").innerHTML = "<img src='img/vs.png'/>";
-       function aleatorio(minimo, maximo){
-            var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );  
-            return numero;
-       }
-       var opcionMaquina = aleatorio(0,4);
-       if (opcionMaquina == 0) {
-        document.getElementById("maquina").innerHTML = "<img src='img/piedra.png'/>";
-        var vpiedra=0;
-       }else if (opcionMaquina == 1) {
-        document.getElementById("maquina").innerHTML = "<img src='img/papel.png'/>";
-        var vpapel=1;
-       }else if (opcionMaquina == 2) {
-        document.getElementById("maquina").innerHTML = "<img src='img/tijera.png'/>";
-        var vtijera=2;
-       }else if (opcionMaquina == 3) {
-        document.getElementById("maquina").innerHTML = "<img src='img/lagarto.png'/>";
-        var vlagarto = 3;
-       }else if (opcionMaquina == 4) {
-        document.getElementById("maquina").innerHTML = "<img src='img/spock.png'/>";
-        var vspock = 4;
-        }
+}
 
-        if (vpiedra == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>";
-        } else if (vpapel == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>";
-        } else if (vtijera == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/empate.png'/>";
-        } else if (vlagarto == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>";
-        } else if (vspock == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>"; 
-        }     
-
-  }
-  function LagartoFunction() {
-       document.getElementById("usuario").innerHTML = "<img src='img/lagarto.png'/>";
-       document.getElementById("vs").innerHTML = "<img src='img/vs.png'/>";
-       function aleatorio(minimo, maximo){
-            var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );  
-            return numero;
-       }
-       var opcionMaquina = aleatorio(0,4);
-       if (opcionMaquina == 0) {
-        document.getElementById("maquina").innerHTML = "<img src='img/piedra.png'/>";
-        var vpiedra=0;
-       }else if (opcionMaquina == 1) {
-        document.getElementById("maquina").innerHTML = "<img src='img/papel.png'/>";
-        var vpapel=1;
-       }else if (opcionMaquina == 2) {
-        document.getElementById("maquina").innerHTML = "<img src='img/tijera.png'/>";
-        var vtijera=2;
-       }else if (opcionMaquina == 3) {
-        document.getElementById("maquina").innerHTML = "<img src='img/lagarto.png'/>";
-        var vlagarto = 3;
-       }else if (opcionMaquina == 4) {
-        document.getElementById("maquina").innerHTML = "<img src='img/spock.png'/>";
-        var vspock = 4;
-        }
-
-        if (vpiedra == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>";
-        } else if (vpapel == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>";
-        } else if (vtijera == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>";
-        } else if (vlagarto == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/empate.png'/>";
-        } else if (vspock == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>"; 
-        }     
-  }
-  function SpockFunction() {
-       document.getElementById("usuario").innerHTML = "<img src='img/spock.png'/>";
-       document.getElementById("vs").innerHTML = "<img src='img/vs.png'/>";
-       function aleatorio(minimo, maximo){
-            var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );  
-            return numero;
-       }
-       var opcionMaquina = aleatorio(0,4);
-       if (opcionMaquina == 0) {
-        document.getElementById("maquina").innerHTML = "<img src='img/piedra.png'/>";
-        var vpiedra=0;
-       }else if (opcionMaquina == 1) {
-        document.getElementById("maquina").innerHTML = "<img src='img/papel.png'/>";
-        var vpapel=1;
-       }else if (opcionMaquina == 2) {
-        document.getElementById("maquina").innerHTML = "<img src='img/tijera.png'/>";
-        var vtijera=2;
-       }else if (opcionMaquina == 3) {
-        document.getElementById("maquina").innerHTML = "<img src='img/lagarto.png'/>";
-        var vlagarto = 3;
-       }else if (opcionMaquina == 4) {
-        document.getElementById("maquina").innerHTML = "<img src='img/spock.png'/>";
-        var vspock = 4;
-        }
-
-        if (vpiedra == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>";
-        } else if (vpapel == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>";
-        } else if (vtijera == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/ganas.png'/>";
-        } else if (vlagarto == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/pierdes.png'/>";
-        } else if (vspock == opcionMaquina) {
-          document.getElementById("resultado").innerHTML = "<img class='imgresultado' src='img/empate.png'/>"; 
-        }         
-  }
-
- 
-
+});
